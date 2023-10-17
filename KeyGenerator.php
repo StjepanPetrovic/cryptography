188bcabd7d5909f113adbb91eeed2466c28e2192
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-10-17 21:59:18 +0200

    Add logic for asymmetric keys generation

--- KeyGenerator.php
+++ KeyGenerator.php
@@ -3,6 +3,13 @@
 declare(strict_types=1);
 
 const SYMMETRIC_KEY_FILE = __DIR__ . '/keys/tajni_kljuc.txt';
+const PRIVATE_KEY_FILE = __DIR__ . '/keys/privatni_kljuc.txt';
+const PUBLIC_KEY_FILE = __DIR__ . '/keys/javni_kljuc.txt';
+
+const PRIVATE_KEY_CONFIGURATION = [
+    'private_key_bits' => 2048,
+    'private_key_type' => OPENSSL_KEYTYPE_RSA,
+];
 
 final class KeyGenerator
 {
@@ -10,6 +17,7 @@ final class KeyGenerator
     {
         $this->generateSymmetricKey();
 
+        $this->generateAsymmetricKeys();
     }
 
     private function generateSymmetricKey(): void
@@ -23,7 +31,24 @@ final class KeyGenerator
         echo 'Symmetric key is generated and stored in file.' . PHP_EOL;
     }
 
+    private function generateAsymmetricKeys(): void
     {
+        $asymmetricKey = openssl_pkey_new(PRIVATE_KEY_CONFIGURATION);
+
+        if (!$asymmetricKey instanceof OpenSSLAsymmetricKey){
+            throw new RuntimeException('Could not generate private key.');
         }
+
+        if (!openssl_pkey_export_to_file($asymmetricKey, PRIVATE_KEY_FILE)) {
+            throw new RuntimeException('Could not save private key in file.');
+        }
+
+        $publicKey = openssl_pkey_get_details($asymmetricKey)['key'];
+
+        if (!file_put_contents(PUBLIC_KEY_FILE, $publicKey)) {
+            throw new RuntimeException('Could not save public key in file.');
+        }
+
+        echo 'Private key and public key are generated and stored in files.' . PHP_EOL;
     }
 }
--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Used for symmetric key generation:
 
 `random_bytes` https://www.php.net/manual/en/function.random-bytes.php
 
+## Asymmetric Encryption (RSA cryptopsystem)
+
+Used for asymmetric key generation:
+
+`openssl_pkey_new` https://www.php.net/manual/en/function.openssl-pkey-new.php
+
+`openssl_pkey_export_to_file` https://www.php.net/manual/en/function.openssl-pkey-export-to-file.php
+
+`openssl_pkey_get_details` https://www.php.net/manual/en/function.openssl-pkey-get-details.php
