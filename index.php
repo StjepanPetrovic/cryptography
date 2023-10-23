ed5b09637d5b12534f30c1ef1fe98991e4af924e
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-10-23 18:36:40 +0200

    Add form for sending file to server on user interface

--- index.php
+++ index.php
@@ -26,15 +26,26 @@ $keyGenerator = new KeyGenerator();
             </label>
             <input type="submit" value="Encrypt and Download file">
         </form>
-        <div class="container-recipient-data">
-            <h4>Recipient symmetric key:</h4>
-            <p>
-                <?= $keyGenerator->getKey(KeyGenerator::SYMMETRIC_KEY_FILE) ?>
-            </p>
-            <h4>Recipient public key:</h4>
-            <p>
-                <?= $keyGenerator->getKey(KeyGenerator::PUBLIC_KEY_FILE) ?>
-            </p>
+        <div class="container-server">
+            <div class="server-data">
+                <h4>Server symmetric key:</h4>
+                <p>
+                    <?= $keyGenerator->getKey(KeyGenerator::SYMMETRIC_KEY_FILE) ?>
+                </p>
+                <h4>Server public key:</h4>
+                <p>
+                    <?= $keyGenerator->getKey(KeyGenerator::PUBLIC_KEY_FILE) ?>
+                </p>
+            </div>
+            <div class="server-send-form">
+                <form id="send-form" class="form" action="" method="post" enctype="multipart/form-data">
+                    <label>
+                        File to send:
+                        <input id="file_to_send" type="file" name="file_to_send">
+                    </label>
+                    <input type="submit" value="Send file to server">
+                </form>
+            </div>
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
